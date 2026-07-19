1fccf43c65776a8234cc7a861610d3cae3c3984c
Cyber Net <ssk717747@gmail.com>
2026-07-20 00:22:09 +0600

    Validate BOT_TOKEN, CLIENT_KEY, and CLIENT_SECRET
    Add validation for required environment variables

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CLIENT_KEY = os.environ.get("CLIENT_KEY")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN environment variable not set!")
+if not CLIENT_KEY:
+    raise ValueError("CLIENT_KEY environment variable not set!")
+if not CLIENT_SECRET:
+    raise ValueError("CLIENT_SECRET environment variable not set!")
